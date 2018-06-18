8941a36dd2855983c4b5cc79e33d6d787233ce9b
PengXiao <eagle.xiao@gmail.com>
2018-06-18 23:08:38 +1000

    Try to consumer test-lib3. Browser complains -
    test-lib3.umd.js?892c:241 Uncaught TypeError: Cannot read property 'bind' of undefined
        at eval (test-lib3.umd.js?892c:241)
        at eval (test-lib3.umd.js?892c:252)
        at webpackUniversalModuleDefinition (test-lib3.umd.js?892c:3)
        at eval (test-lib3.umd.js?892c:10)
        at Object../node_modules/test-lib3/dist/test-lib3.umd.js (app.js:1540)
        at __webpack_require__ (app.js:722)
        at fn (app.js:99)
        at eval (main.js:6)
        at Module../src/main.js (app.js:1889)
        at __webpack_require__ (app.js:722)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
 import Vue from 'vue'
 import App from './App.vue'
+import { Version } from 'test-lib3'
 
+console.log(Version)
 Vue.config.productionTip = false
 
 new Vue({
